02132354561876d4c5ee8e0555e61c278bc30134
alston-hsu <alston.hsu@outlook.com>
2020-03-10 16:17:17 -0500

    Completed setup, added create board test

---
+++ src/test/java/config/TrelloApiConfig.java
@@ -0,0 +1,43 @@
+package config;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.annotations.BeforeClass;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class TrelloApiConfig {
+
+    public static Properties prop = new Properties();
+    RequestSpecification trello_requestSpec;
+    ResponseSpecification trello_responseSpec;
+
+    public static String getPropertyValue(String propertyKey) throws IOException {
+        InputStream file = new FileInputStream(".secret/creds.properties");
+        prop.load(file);
+        return prop.getProperty(propertyKey);
+    }
+
+    @BeforeClass
+    public void setup() {
+        trello_requestSpec = new RequestSpecBuilder()
+                .setBaseUri("https://api.trello.com")
+                .setBasePath("/1")
+                .addFilter(new ResponseLoggingFilter())
+                .build();
+
+        trello_responseSpec = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .build();
+
+        RestAssured.requestSpecification = trello_requestSpec;
+        RestAssured.responseSpecification = trello_responseSpec;
+    }
+}
